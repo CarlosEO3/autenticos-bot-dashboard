345d6352bcfff5c35a307dc906a43c67b3ce671d
Autenticos <carlose.orozcob@gmail.com>
2026-09-10 15:59:04 -0500

    Mejora de PWA para instalación en celulares

--- vite.config.js
+++ vite.config.js
@@ -23,14 +23,20 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: '/favicon.svg',
+            src: '/logo-azul.png',
             sizes: '192x192',
-            type: 'image/svg+xml'
+            type: 'image/png'
           },
           {
-            src: '/favicon.svg',
+            src: '/logo-azul.png',
             sizes: '512x512',
-            type: 'image/svg+xml'
+            type: 'image/png'
+          },
+          {
+            src: '/logo-azul.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
       }
